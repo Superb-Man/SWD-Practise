578d07f2bae81bdabf36dd7e431fb1793298a2c0
Superb-Man <kazitoriqe207@gmail.com>
2024-01-22 00:24:55 +0600

    eww

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const router = require('./routes/routes');
+const userRouter = require('./routes/uroutes');
+const adminRouter = require('./routes/aroutes');
+// console.log(adminRouter);
+// console.log(userRouter);
 const cors = require('cors');
 
 dotenv.config();
@@ -10,7 +13,8 @@ const port = process.env.port;
 
 app.use(express.json());
 app.use(cors());
-app.use('/user', router);
+app.use('/user', userRouter);
+app.use('/admin', adminRouter);
 
 app.get('/', (req, res) => {
     res.send('Account service is up and running');
