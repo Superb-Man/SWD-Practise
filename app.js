bdd04b4506cb4dde2f4b65c7de40fc3eb7359e5e
Superb-Man <kazitoriqe207@gmail.com>
2024-01-21 12:44:26 +0600

    sad

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const router = require('./routes/routes');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.port;
+
+app.use(express.json());
+app.use(cors());
+app.use('/user', router);
+
+app.get('/', (req, res) => {
+    res.send('Account service is up and running');
+});
+
+app.listen(port, () => {
+    console.log(`Account service listening on port ${port}`);
+});
---
+++ routes/routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser').json();
+const userController = require('../controller/user');
+
+const router = express.Router();
+
+//test
+router.get('/getinfotest', bodyParser,userController.getInfo_test);
+//it will be deleted later!
+
+router.get('/signup', bodyParser,userController.userSignUp);
+router.get('/login', bodyParser,userController.userLogin);
+router.get('/passwordResetReq', bodyParser,userController.userPasswordResetReq);
+router.get('/updatePassword', bodyParser,userController.userUpdatePassword);
+module.exports = router;
