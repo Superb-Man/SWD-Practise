a45c2d058f57f4ded55bd5627e6b1070b09e2946
Superb-Man <kazitoriqe207@gmail.com>
2024-02-09 03:43:32 +0600

    admin_add_schedule done+booking of ticket

--- Train-Service/controller/user/booking.js
+++ Train-Service/controller/user/booking.js
@@ -1,63 +1,71 @@
-// const dotenv = require('dotenv');
-// const crypt = require('crypto-js');
-// const jwt = require('jsonwebtoken');
-// const accountPool = require('../../config/accountDB.js');
-// const airPool = require('../../config/airDB.js');
-// const crypto = require('../../utils.js');
-// const { query } = require('express');
+const dotenv = require('dotenv');
+const crypt = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const accountPool = require('../../config/accountDB.js');
+const trainPool = require('../../config/trainDB.js');
+const crypto = require('../../utils.js');
+const { query } = require('express');
 
-// dotenv.config();
+dotenv.config();
 
-// //before payment transaction
-// const temporarySeatBooking = async (req, res) => {
-//     //authentication lagbe
-//     req.body.user_name = 'X-33' ;
-//     req.body.schedule_id = 1 ;
-//     req.body.class_name = 'economy' ;
-//     req.body.booked_deatils = [[4,0,1],[2,1,1],[5,2,1]] ;
-//     obj = req.body ;
+//before payment transaction
+const temporarySeatBooking = async (req, res) => {
+    //authentication lagbe
+    req.body.user_name = 'X-33' ;
+    req.body.schedule_id = 1 ;
+    req.body.class_name = 'SHOVAN' ;
+    req.body.booked_deatils = [[2,0,,2,1],[2,1,2,1],[2,2,2,1]] ;
+    obj = req.body ;
 
-//     //transaction starting
-//     try{
-//         const query1 = {
-//             text : 'SELECT class_id FROM "class_info" WHERE class_name = $1',
-//             values : [obj.class_name]
-//         }
-//         let class_id = (await airPool.query(query1)).rows[0].class_id;
-//         class_id-=1 ;
+    //transaction starting
+    try{
+        trainPool.query('BEGIN') ;
+        const query1 = {
+            text : 'SELECT coach_id FROM "coach_info" WHERE coach_name = $1',
+            values : [obj.class_name]
+        }
+        let coach_id = (await trainPool.query(query1)).rows[0].coach_id;
+        coach_id-=1 ;
     
-//         console.log(class_id) ;
-//         airPool.query('BEGIN') ;
-//         console.log("temporarySeatBooking") ;
-//         console.log(obj) ;
-//         const dimension_query = {
-//             text : 'SELECT "air_details".dimensions FROM "air_details" JOIN "air_schedule_info" ON "air_schedule_info".air_id = "air_details".air_id  WHERE schedule_id = $1',
-//             values : [obj.schedule_id]
-//         }
+        console.log(coach_id) ;
+        trainPool.query('BEGIN') ;
+        console.log("temporarySeatBooking") ;
+        console.log(obj) ;
+        const dimension_query = {
+            text : 'SELECT "train_details".dimensions,"train_details".coaches FROM "train_details" JOIN "train_schedule_info" ON "train_schedule_info".train_id = "train_details".train_id  WHERE schedule_id = $1',
+            values : [obj.schedule_id]
+        }
 
-//         const dimension = (await airPool.query(dimension_query)).rows[0].dimensions[class_id] ;
-//         console.log(class_id) ;
-//         for(let i = 0 ; i < obj.booked_deatils.length ; i++) {
-//             // console.log(dimension[1]*obj.booked_deatils[i][0]+obj.booked_deatils[i][1]) ;
-//             const query2 = {
-//                 text : 'UPDATE "air_schedule_info" SET seat_details[$1][$2][$3] = $4 WHERE schedule_id = $5',
-//                 values : [class_id+1,dimension[1]*obj.booked_deatils[i][0]+obj.booked_deatils[i][1]+1,3,1,obj.schedule_id]
-//             }
-//             await airPool.query(query2) ;
-//         }
-//         await airPool.query('COMMIT') ;
+        let dimension = -1 ;
+        let c_id = -1 ;
+        const {dimensions,coaches} = (await trainPool.query(dimension_query)).rows[0] ;
+        for (let i = 0 ; i < coaches.length ; i++) {
+            if(coaches[i] == coach_id) {
+                dimension = dimensions[i] ;
+                c_id = i+1 ;
+                break ;
+            }
+        }
+        for(let i = 0 ; i < obj.booked_deatils.length ; i++) {
+            // console.log(dimension[1]*obj.booked_deatils[i][0]+obj.booked_deatils[i][1]) ;
+            const query2 = {
+                text : 'UPDATE "train_schedule_info" SET seat_details[$1][$2][$3] = $4 WHERE schedule_id = $5',
+                values : [c_id+1,dimension[1]*obj.booked_deatils[i][0]+obj.booked_deatils[i][1]+1,3,1,obj.schedule_id]
+            }
+            await trainPool.query(query2) ;
+        }
+        await trainPool.query('COMMIT') ;
+        res.status(200).json({message: "Seat Booked"}) ;
 
-//         res.status(200).json({message: "Seat Booked"}) ;
 
+    }catch(err) {
+        await trainPool.query('ROLLBACK') ;
+        console.log(err) ;
+        res.status(500).json({message: "Internal Server Error"}) ;
+    }
 
-//     }catch(err) {
-//         await airPool.query('ROLLBACK') ;
-//         console.log(err) ;
-//         res.status(500).json({message: "Internal Server Error"}) ;
-//     }
+};
 
-// };
-
-// module.exports = {
-//     temporarySeatBooking
-// }
+module.exports = {
+    temporarySeatBooking
+}
